da443aa4bbf8cbde82d48430009ea8f9c4706362
Adam McMaster <adam.mcmaster@open.ac.uk>
2022-03-08 14:29:37 +0000

    Add British Science Week landing page

--- vespa/urls.py
+++ vespa/urls.py
@@ -19,7 +19,13 @@ from django.urls import path
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic import DetailView
-from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView
+from django.views.generic.dates import (
+    ArchiveIndexView,
+    YearArchiveView,
+    MonthArchiveView,
+    DayArchiveView,
+    DateDetailView,
+)
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -32,63 +38,120 @@ import starcatalogue.views
 import waspstatic.views
 
 blog_context = {
-    'categories': blog.models.Category.objects.all(),
-    'years': blog.models.Article.objects.all().datetimes('date_created', 'year')
+    "categories": blog.models.Category.objects.all(),
+    "years": blog.models.Article.objects.all().datetimes("date_created", "year"),
 }
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-
-    path('about/', waspstatic.views.AboutView.as_view(), name='about'),
-    path('exoplanets/', TemplateView.as_view(template_name='waspstatic/exoplanets.html'), name='exoplanets'),
-    path('black-hole-hunters/', TemplateView.as_view(template_name='waspstatic/black-hole-hunters.html'), name='black-hole-hunters'),
-
-    path('vespa/', starcatalogue.views.StarListView.as_view(template_name = 'starcatalogue/index.html'), name='vespa'),
-    path('vespa/browse/', starcatalogue.views.StarListView.as_view(), name='browse'),
-    path('vespa/data-releases/', ListView.as_view(
-        queryset=starcatalogue.exports.DataExport.objects.filter(in_data_archive=True).order_by('-created'),
-        template_name='starcatalogue/data_releases.html'
-    ), name='data_releases'),
-    path('vespa/export/', starcatalogue.views.GenerateExportView.as_view(), name='generate_export'),
-    path('vespa/export/<str:pk>/', DetailView.as_view(model=starcatalogue.models.DataExport), name='view_export'),
-    path('vespa/source/<str:swasp_id>/', starcatalogue.views.SourceView.as_view(), name='view_source'),
-
-    path('blog/', ArchiveIndexView.as_view(
-        model=blog.models.Article,
-        paginate_by=10,
-        date_field="date_created",
-        extra_context=blog_context,
-    ), name='blog'),
-    path('blog/feed/', blog.feeds.ArticlesFeed(), name='blog_feed'),
-    path('blog/<int:year>/', YearArchiveView.as_view(
-        model=blog.models.Article,
-        paginate_by=10,
-        date_field="date_created",
-        make_object_list=True,
-        extra_context=blog_context,
-    ), name='blog'),
-    path('blog/<int:year>/<str:month>/', MonthArchiveView.as_view(
-        model=blog.models.Article,
-        paginate_by=10,
-        date_field="date_created",
-        extra_context=blog_context,
-    ), name='blog'),
-    path('blog/<int:year>/<str:month>/<int:day>/', DayArchiveView.as_view(
-        model=blog.models.Article,
-        paginate_by=10,
-        date_field="date_created",
-        extra_context=blog_context,
-    ), name='blog'),
-    path('blog/<int:year>/<str:month>/<int:day>/<str:slug>/', DateDetailView.as_view(
-        model=blog.models.Article,
-        date_field="date_created",
-        allow_future=True,
-        extra_context=blog_context,
-    ), name='blog_article_detail'),
-        path('blog/<str:category>/', blog.views.CategoryListView.as_view(
-        paginate_by=10,
-        extra_context=blog_context,
-    ), name='blog'),
-
-    path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
+    path("about/", waspstatic.views.AboutView.as_view(), name="about"),
+    path(
+        "exoplanets/",
+        TemplateView.as_view(template_name="waspstatic/exoplanets.html"),
+        name="exoplanets",
+    ),
+    path(
+        "black-hole-hunters/",
+        TemplateView.as_view(template_name="waspstatic/black-hole-hunters.html"),
+        name="black-hole-hunters",
+    ),
+    path(
+        "britishscienceweek/",
+        TemplateView.as_view(template_name="waspstatic/bsw2022.html"),
+        name="british-science-week",
+    ),
+    path(
+        "vespa/",
+        starcatalogue.views.StarListView.as_view(
+            template_name="starcatalogue/index.html"
+        ),
+        name="vespa",
+    ),
+    path("vespa/browse/", starcatalogue.views.StarListView.as_view(), name="browse"),
+    path(
+        "vespa/data-releases/",
+        ListView.as_view(
+            queryset=starcatalogue.exports.DataExport.objects.filter(
+                in_data_archive=True
+            ).order_by("-created"),
+            template_name="starcatalogue/data_releases.html",
+        ),
+        name="data_releases",
+    ),
+    path(
+        "vespa/export/",
+        starcatalogue.views.GenerateExportView.as_view(),
+        name="generate_export",
+    ),
+    path(
+        "vespa/export/<str:pk>/",
+        DetailView.as_view(model=starcatalogue.models.DataExport),
+        name="view_export",
+    ),
+    path(
+        "vespa/source/<str:swasp_id>/",
+        starcatalogue.views.SourceView.as_view(),
+        name="view_source",
+    ),
+    path(
+        "blog/",
+        ArchiveIndexView.as_view(
+            model=blog.models.Article,
+            paginate_by=10,
+            date_field="date_created",
+            extra_context=blog_context,
+        ),
+        name="blog",
+    ),
+    path("blog/feed/", blog.feeds.ArticlesFeed(), name="blog_feed"),
+    path(
+        "blog/<int:year>/",
+        YearArchiveView.as_view(
+            model=blog.models.Article,
+            paginate_by=10,
+            date_field="date_created",
+            make_object_list=True,
+            extra_context=blog_context,
+        ),
+        name="blog",
+    ),
+    path(
+        "blog/<int:year>/<str:month>/",
+        MonthArchiveView.as_view(
+            model=blog.models.Article,
+            paginate_by=10,
+            date_field="date_created",
+            extra_context=blog_context,
+        ),
+        name="blog",
+    ),
+    path(
+        "blog/<int:year>/<str:month>/<int:day>/",
+        DayArchiveView.as_view(
+            model=blog.models.Article,
+            paginate_by=10,
+            date_field="date_created",
+            extra_context=blog_context,
+        ),
+        name="blog",
+    ),
+    path(
+        "blog/<int:year>/<str:month>/<int:day>/<str:slug>/",
+        DateDetailView.as_view(
+            model=blog.models.Article,
+            date_field="date_created",
+            allow_future=True,
+            extra_context=blog_context,
+        ),
+        name="blog_article_detail",
+    ),
+    path(
+        "blog/<str:category>/",
+        blog.views.CategoryListView.as_view(
+            paginate_by=10,
+            extra_context=blog_context,
+        ),
+        name="blog",
+    ),
+    path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
